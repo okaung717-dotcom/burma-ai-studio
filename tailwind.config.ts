79fadc103a8f61aa2d12a868bbc48871f50d57b0
Kaung Satt OO <kaungsattoo@Kaungs-MacBook-Air.local>
2026-06-20 21:31:27 +0630

    Fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,11 +6,12 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./*.{js,ts,jsx,tsx,mdx}", // ဒီစာကြောင်းလေး ထည့်ပေးပါ
+    "./*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
 export default config;
