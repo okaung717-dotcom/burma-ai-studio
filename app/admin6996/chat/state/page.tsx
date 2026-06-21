56126754f053eb3b303a818d37de47c21333cffe
okaung717-dotcom <okaung717@gmail.com>
2026-06-22 04:01:40 +0630

    Add chat status panel route

---
+++ app/admin6996/chat/state/page.tsx
@@ -0,0 +1,9 @@
+import StatePanel from "../StatePanel";
+
+export default function ChatStatePage() {
+  return (
+    <main className="min-h-screen bg-[#fff9f0] px-5 py-10 text-[#1a0b0e] dark:bg-[#100708] dark:text-[#fff7eb]">
+      <StatePanel />
+    </main>
+  );
+}
