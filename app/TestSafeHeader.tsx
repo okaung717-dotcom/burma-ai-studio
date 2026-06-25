db88c195b4b49e45afe4b1bcc5066f62c38886ed
okaung717-dotcom <okaung717@gmail.com>
2026-06-25 23:52:24 +0630

    Expand test header

--- app/TestSafeHeader.tsx
+++ app/TestSafeHeader.tsx
@@ -1,1 +1,7 @@
-export default function TestSafeHeader() { return <div>Header</div>; }
+"use client";
+
+import Link from "next/link";
+
+export default function TestSafeHeader() {
+  return <header><Link href="/">Burma AI</Link></header>;
+}
