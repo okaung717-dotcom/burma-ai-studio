6ca8e3ab402ddacdfde089ffdda10b9510a18a2e
okaung717-dotcom <okaung717@gmail.com>
2026-06-25 17:47:33 +0630

    Show free app install prompt on public site

--- app/AppShell.tsx
+++ app/AppShell.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import Navbar from "./Navbar";
 import AIAssistant from "./AIAssistant";
 import AnalyticsTracker from "./AnalyticsTracker";
+import InstallAppPrompt from "./InstallAppPrompt";
 
 export default function AppShell({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
@@ -16,6 +17,7 @@ export default function AppShell({ children }: { children: React.ReactNode }) {
       <main className="w-full flex-grow">
         {children}
       </main>
+      {!isAdminArea && <InstallAppPrompt />}
       {!isAdminArea && <AIAssistant />}
     </>
   );
