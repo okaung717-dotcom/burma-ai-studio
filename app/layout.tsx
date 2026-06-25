5c7bbfb8e634411b8ebfc3efbe4abc05a7af1c54
okaung717-dotcom <okaung717@gmail.com>
2026-06-26 00:47:40 +0630

    Load contrast fixes after global theme styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./contrast-fixes.css";
 import AppShell from "./AppShell";
 import { LanguageProvider } from "./LanguageContext";
 import { ThemeProvider } from "./ThemeProvider";
