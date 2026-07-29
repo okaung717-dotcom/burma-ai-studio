334e41c4f6e0a381149fe3f992e4b451206d6248
okaung717-dotcom <okaung717@gmail.com>
2026-07-30 00:43:10 +0630

    Load persistent history before website chat mounts

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -1,11 +1,13 @@
 import type { ReactNode } from "react";
 import "./chat-cleanup.css";
 import "./chat-myanmar-typography-fix.css";
+import ChatHistoryPersistence from "./ChatHistoryPersistence";
 import ChatMyanmarTypographyBridge from "./ChatMyanmarTypographyBridge";
 
 export default function ChatLayout({ children }: { children: ReactNode }) {
   return (
     <>
+      <ChatHistoryPersistence />
       <ChatMyanmarTypographyBridge />
       {children}
     </>
